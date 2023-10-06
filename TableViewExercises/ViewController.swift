05a3e60540bd7fb8cf0ba62cb2449db7b462a25f
msnbek <mahmutsenbek@icloud.com>
2023-10-06 10:59:55 +0300

    Initial Commit

---
+++ TableViewExercises/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewExercises
+//
+//  Created by Mahmut Senbek on 6.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
